5ec617c7220b77199b2e6f1eae82219577591ca4
Lucile-Prothon <lucile.prothon@edu.univ-fcomte.fr>
2026-09-16 08:43:07 +0200

    Ajout du menu en onglet pour les species

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  adapter: netlify()
+  }
 });
